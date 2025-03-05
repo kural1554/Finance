116553329d3c3caac66503e8a4dd84b41db06079
Santhosh29k <thanush9524@gmail.com>
2025-03-05 09:57:25 +0530

    finalform

--- Admin/src/pages/Chat/Chat.js
+++ Admin/src/pages/Chat/Chat.js
@@ -283,7 +283,7 @@ function DatatableTables() {
                         <Card>
                         <CardHeader className="d-flex justify-content-between align-items-center">
                                 <h4 className="mb-0">Loan Management</h4>
-                                <Button color="primary" onClick={() => navigate('/new-loan')}>
+                                <Button color="primary" onClick={() => navigate('/form-wizard')}>
                                 <FeatherIcon icon="plus-circle" className="me-2" />New Loan</Button>
                             </CardHeader>
                             <CardBody>
